54cbaea9355ecfef229b01d8b6789fb40684ed17
Sonata0xff <2080782895@qq.com>
2025-12-17 23:32:15 +0800

    upload project

---
+++ Users/main.c
@@ -0,0 +1,21 @@
+#include "stm32f4xx.h"                  // Device header
+#include "stm32f4xx_hal.h"              // Keil::Device:STM32Cube HAL:Common
+
+int main()
+{
+	GPIO_InitTypeDef gpio = {
+		.Pin = GPIO_PIN_6,
+		.Mode = GPIO_MODE_OUTPUT_PP,
+		.Pull = GPIO_NOPULL,
+		.Speed = GPIO_SPEED_FREQ_MEDIUM
+	};
+	HAL_Init();
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	HAL_GPIO_Init(GPIOA, &gpio);
+	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
+	while(1) {
+		HAL_Delay(500);
+		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
+	}
+	return 0;
+}
