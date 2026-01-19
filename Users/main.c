8ad797c4c8227de52816abab584de7b42ea1ec29
Sonata0xff <2080782895@qq.com>
2026-01-19 21:36:38 +0800

    fix

--- Users/main.c
+++ Users/main.c
@@ -1,5 +1,5 @@
 #include "stm32f4xx.h"                  // Device header
-#include "stm32f4xx_hal.h"              // Keil::Device:STM32Cube HAL:Common
+#include "stm32f4xx_hal.h"              
 
 int main()
 {
