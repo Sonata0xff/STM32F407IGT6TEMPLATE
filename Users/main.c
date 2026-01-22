69182a5ea747fa264b343906e28270ba43fb6db6
Sonata0xff <2080782895@qq.com>
2026-01-22 20:45:25 +0800

    add I2C template

--- Users/main.c
+++ Users/main.c
@@ -10,28 +10,13 @@ int main()
 	StandardProcess();
 	
 	//data def
-	//unsigned char title[] = "hello,407!";
-	char data[] = {0xaa};
+	unsigned char title[] = "hello,407!";
 	//process
 	IIC1_Init(0, 0);
-	IIC1SendBytes(data, 1, 0xaa);
-	//OLED_Init();
-	//OLED_TurnOn_Screen();
-	//OLED_Flash_Screen(0x00);
-	//OLED_WriteIn_16x8String(0, 0, 10, title);
+	OLED_Init();
+	OLED_TurnOn_Screen();
+	OLED_Flash_Screen(0x00);
+	OLED_WriteIn_16x8String(0, 0, 10, title);
 	while(1);
-	/*GPIO_InitTypeDef gpio = {
-		.Pin = GPIO_PIN_6,
-		.Mode = GPIO_MODE_OUTPUT_PP,
-		.Pull = GPIO_NOPULL,
-		.Speed = GPIO_SPEED_FREQ_MEDIUM
-	};
-	__HAL_RCC_GPIOA_CLK_ENABLE();
-	HAL_GPIO_Init(GPIOA, &gpio);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6, GPIO_PIN_SET);
-	while(1) {
-		HAL_Delay(500);
-		HAL_GPIO_TogglePin(GPIOA, GPIO_PIN_6);
-	}*/
 	return 0;
 }
